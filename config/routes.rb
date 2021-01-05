6e6a65cf0528e24e2c87b628aa12314961e66994
Farzan508 <bsef17m508@pucit.edu.pk>
2021-01-05 22:29:19 +0500

    Integrated index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
+
   get 'pages/contact'
+
   get 'pages/about'
+
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My blog post #{blog}",
+		body:"qwertyuiopasdfghjkl;zxcvbnm"
+	)
+end
+
+puts "10 blog posts created!!"
+
+5.times do |skill|
+	Skill.create!(
+		title:"Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 blog posts created!!"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Porfolio #{portfolio_item}",
+		subtitle: "subtitle",
+		body: "qwertyuiosdfghjk",
+		thumb_image: "https://via.placeholder.com/150"
+	)
+
+end
